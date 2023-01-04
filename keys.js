36e3e00286e5f4ba25f4d56d3067043a3ebb4f4c
Pranav Patil <83356594+Pranav122002@users.noreply.github.com>
2023-01-04 20:02:52 +0530

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoUrl: "mongodb+srv://Patil122002:FSIbcBJFpkZB0ii7@cluster0.p3ixoej.mongodb.net/?retryWrites=true&w=majority",
+    mongoUrl: "",
     Jwt_secret: "padlkcrcvnecfw"
 
 };
